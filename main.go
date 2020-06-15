70ea20df2d583fa520f477c24b91499e8d8a5f47
Michael Christenson II <Michael.Christenson.II@ibm.com>
2020-06-15 08:15:19 -0400

    Use light theme

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/theme"
 
 	"github.com/allthingsexperimental/gametime/view"
 )
 
 func main() {
 	a := app.New()
+	a.Settings().SetTheme(theme.LightTheme())
 	w := a.NewWindow("Game Time!")
 	w.SetContent(view.Render())
 	w.ShowAndRun()
