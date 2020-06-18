9760929fbd050ff56e317bb6a400c7d0dbb91844
Michael Christenson II <Michael.Christenson.II@ibm.com>
2020-06-18 17:54:04 -0400

    beginning the toolbar creation

--- view/actionBar/render.go
+++ view/actionBar/render.go
@@ -2,11 +2,14 @@ package actionBar
 
 import (
 	"fyne.io/fyne"
-	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
 )
 
 func Render(props map[string]interface{}) fyne.CanvasObject {
-	return fyne.NewContainerWithLayout(
-		layout.NewHBoxLayout(),
+	gear, _ := fyne.LoadResourceFromURLString("https://banner2.cleanpng.com/20180410/xkq/kisspng-computer-icons-download-gears-5accce884910e3.2300818315233716562993.jpg")
+
+	t := widget.NewToolbar(
+		widget.NewToolbarAction(gear, func() {}),
 	)
+	return t
 }
